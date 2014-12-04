42cb2c234546848113f8cc3cb2a3ba5b702a0d45
Warren <warren.ali@gmail.com>
2014-12-04 17:44:35 +0000

    Project Structure Updates
    __init__ files

--- scrapers/members_interests.py
+++ scrapers/members_interests.py
@@ -2,35 +2,58 @@ from data_interfaces import mongo
 from lxml import objectify
 import os
 
-categories = [
-    "Directorships",
-    "Remunerated employment, office, profession etc",
-    "Clients",
-    "Sponsorships",
-    "Overseas visits",
-    "Land and Property",
-    "Shareholdings",
-    "Miscellaneous"
-]
+cache = mongo.MongoInterface()
 
 
 def parse_xml(xml_path, file_name):
     with open(xml_path + file_name) as f:
             xml = f.read()
     root = objectify.fromstring(xml)
+    contents = []
     for mp in root.getchildren():
         print mp.attrib["membername"]
+        categories = []
         for category in mp.getchildren():
             if "name" in category.attrib:
-                if category.attrib["name"] == "Remunerated employment, office, profession etc":
-                    parse_category(category)
+                if "." in category.attrib["name"]:
+                    category_name = category.attrib["name"].replace(".", "_")
+                else:
+                    category_name = category.attrib["name"]
+                cat_data = {category_name: parse_category(category)}
+                categories.append(cat_data)
         print "\n---"
+        mp_data = {mp.attrib["membername"]: categories}
+        contents.append(mp_data)
+    file_name = file_name.split(".")[0]
+    data = {file_name: contents}
+    cache.db.mps_interests.save(data)
 
 
 def parse_category(category):
     print "\t ", category.attrib["name"]
+    items = []
     for item in category.getchildren():
-        print "\t\t", item.text
+        if item.getchildren():
+            text = parse_item(item)
+            items.append(text)
+        else:
+            text = item.text
+            items.append(text)
+        print "\t\t", text
+    return items
+
+
+def parse_item(items):
+    string = ""
+    for x in items.getchildren():
+        if x.getchildren():
+            for y in x.getchildren():
+                if y.text is not None:
+                    string += y.text
+        else:
+            if x.text is not None:
+                string += x.text
+    return string.strip()
 
 
 def parse():

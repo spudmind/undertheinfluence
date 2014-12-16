69a2c47a67690d18d79fa4719b05a7e5cfe1b873
Warren <warren.ali@gmail.com>
2014-12-16 18:10:34 +0000

    MP Information import

---
+++ data_interfaces/hansard.py
@@ -0,0 +1,92 @@
+from interfaces import theyworkforyou
+import simplejson
+
+
+class TWFYHansard:
+    def __init__(self, link=False):
+        self.twfy = theyworkforyou.TWFY('ESHFQ9AQWgTuFyCfx7DAWpDc')
+
+    def get_mps(self):
+        return simplejson.loads(
+            self.twfy.api.getMPs(output='js'),
+            'iso-8859-1'
+        )
+
+    def get_mp_details(self, person_id):
+        return simplejson.loads(
+            self.twfy.api.getMP(output='js', id=person_id),
+            'iso-8859-1'
+        )
+
+    def get_mp_debates(self, debate_type, person):
+        return simplejson.loads(
+            self.twfy.api.getDebates(output='js', type=debate_type, person=person),
+            'iso-8859-1'
+        )
+
+    def get_debate(self, debate_id):
+        debate_found = self._get_debate_detail(
+            "commons",
+            debate_id
+        )
+        if debate_found:
+            starting_point = debate_found[0]
+            mp_comment = debate_found[-1]
+            start = {
+                    "date": starting_point["hdate"],
+                    "content_count": starting_point["contentcount"],
+                    "body": starting_point["body"],
+                    "debate_id": starting_point["gid"]
+            }
+            mp_comment = {
+                    "date": mp_comment["hdate"],
+                    "body": mp_comment["body"],
+                    "debate_id": mp_comment["gid"]
+            }
+            if len(debate_found) == 3:
+                sub = debate_found[1]
+                sub_cat = {
+                    "date": sub["hdate"],
+                    "body": sub["body"],
+                    "content_count": sub["contentcount"],
+                    "debate_id": sub["gid"]
+                }
+            else:
+                sub_cat = None
+            return start, sub_cat, mp_comment
+        else:
+            return None
+
+    def get_full_debate(self, full_debate_id, sub_category=True):
+        debate_full = self._get_debate_detail(
+            "commons",
+            full_debate_id
+        )
+        #print '\n\n[o] Getting Full Debate Details [id: %s count:%s]' \
+        #      % (full_debate_id, len(debate_full))
+        main = {
+            "debate_id": debate_full[0]["gid"],
+            "date": debate_full[0]["hdate"],
+            "topic": debate_full[0]["body"]
+        }
+        if sub_category:
+            sub_cat = {
+                "debate_id": debate_full[1]["gid"],
+                "date": debate_full[1]["hdate"],
+                "topic": debate_full[1]["body"]
+            }
+            full_debate = debate_full[2:]
+        else:
+            sub_cat = None
+            full_debate = debate_full[1:]
+        return main, sub_cat, full_debate
+
+    def _get_debate_detail(self, debate_type, debate_id):
+        try:
+            return simplejson.loads(
+                self.twfy.api.getDebates(output='js', type=debate_type, gid=debate_id),
+                'iso-8859-1'
+            )
+        except simplejson.scanner.JSONDecodeError:
+            print "JSONDecodeError"
+            return None
---
+++ scrapers/mps.py
@@ -0,0 +1,168 @@
+from data_interfaces import hansard
+from utils import mongo
+from fuzzywuzzy import process
+import requests
+import os
+
+
+class CacheMPs():
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    ALL_PARTIES_API = 'http://www.theguardian.com/politics/api/party/all/json'
+    VOTE_MATRIX = 'data/votematrix-2010.csv'
+    TEST = None
+
+    def __init__(self):
+        print "Importing MPs"
+        self.fuzzy_match = process
+        self.cache = mongo.MongoInterface()
+        self.requests = requests
+        self.hansard = hansard.TWFYHansard()
+        self.mps = self.hansard.get_mps()
+        self.all_mps = None
+
+    def import_mps(self):
+        #self._get_twfy_data()
+        self._get_guardian_data()
+        self._get_publicwhip_data()
+
+    def _get_twfy_data(self):
+        print "Getting Mps from TWFY"
+        for mp in self.mps:
+            self._print_out("MP", mp["name"])
+            self._print_out("Party", mp["party"])
+            self._print_out("person_id", mp["person_id"])
+            node = {
+                "full_name": mp["name"],
+                "twfy_id": mp["person_id"],
+                "publicwhip_id": None,
+                "party": mp["party"],
+                "guardian_url": None,
+                "publicwhip_url": None,
+                "guardian_image": None
+            }
+            print "\n"
+            details = self.hansard.get_mp_details(mp["person_id"])
+            if details:
+                node["first_name"] = details[0]["first_name"]
+                node["last_name"] = details[0]["last_name"]
+                node["number_of_terms"] = len(details)
+            terms = []
+            for entry in details:
+                term = {
+                    "party": entry["party"],
+                    "constituency": entry['constituency'],
+                    "left_house": entry["left_house"],
+                    "entered_house": entry["entered_house"],
+                    "left_reason": entry["left_reason"]
+                }
+                if "office" in entry:
+                    offices = self._get_office(entry["office"])
+                    if len(offices) > 0:
+                        term["offices_held"] = offices
+                terms.append(term)
+            node["terms"] = terms
+            self._report(node)
+            self.cache.db.scraped_mp_info.save(node)
+            print "---"
+
+    def _get_guardian_data(self):
+        print "Updating Guardian data"
+        self.all_mps = [
+            doc["full_name"] for doc in self.cache.db.scraped_mp_info.find()
+        ]
+        for person in self._iterate_guardian_api():
+            url = person["aristotle-url"]
+            cached = self._find_cached_mp(person["name"])
+            self._update_cached_mp(cached["_id"], "guardian_url", url)
+            if "image" in person:
+                self._update_cached_mp(
+                    cached["_id"], "guardian_image", person["image"]
+                )
+            self._print_out(cached["full_name"], url)
+
+    def _get_publicwhip_data(self):
+        with open(CacheMPs.VOTE_MATRIX) as fin:
+            rows = (line.split('\t') for line in fin)
+            for row in rows:
+                name, id = u'{0} {1}'.format(row[1], row[2]), row[0]
+                url = u"http://publicwhip.com/mp.php?mpid={0}".format(id)
+                result = self._find_cached_mp(name)
+                self._update_cached_mp(result["_id"], "publicwhip_id", id)
+                self._update_cached_mp(result["_id"], "publicwhip_url", url)
+                self._print_out(name, url)
+
+    def _iterate_guardian_api(self):
+        r = self.requests.get(CacheMPs.ALL_PARTIES_API)
+        parties = r.json()["parties"]
+        for party in parties:
+            party_uri = party["json-url"]
+            r = self.requests.get(party_uri)
+            if not r.status_code == 404:
+                mps = r.json()["party"]["mps"]
+                for mp in mps:
+                    mp_uri = mp["json-url"]
+                    r = self.requests.get(mp_uri)
+                    if not r.status_code == 404:
+                        person = r.json()["person"]
+                        yield person
+
+    def _find_cached_mp(self, search):
+        if self.all_mps:
+            cand = self.fuzzy_match.extractOne(search, self.all_mps)
+            name = cand[0]
+        else:
+            name = search
+        result = self.cache.db.scraped_mp_info.find({"full_name": name}).limit(1)
+        try:
+            return result[0]
+        except IndexError:
+            return None
+
+    def _update_cached_mp(self, id, key, value):
+        self.cache.db.scraped_mp_info.update({"_id": id}, {"$set": {key: value}})
+
+    def _get_office(self, positions):
+        offices = []
+        if len(positions) > 1:
+            for position in positions:
+                office = {}
+                if position["dept"]:
+                    office = {"department": position["dept"]}
+                if position["position"]:
+                    office = {"position": position["position"]}
+                offices.append(office)
+        else:
+            office = {}
+            if positions[0]["dept"]:
+                office = {"department": positions[0]["dept"]}
+            if positions[0]["position"]:
+                office = {"position": positions[0]["position"]}
+            offices.append(office)
+        if len(offices) == 0:
+            return None
+        else:
+            return offices
+
+    def _report(self, node):
+        for x in node:
+                if x == "terms":
+                    for term in node["terms"]:
+                        print "-"
+                        for y in term:
+                            if y == "offices_held":
+                                offices = term["offices_held"]
+                                if len(offices) > 1 and offices != "none":
+                                    for office in offices:
+                                        for z in office:
+                                            self._print_out(z, office[z])
+                                else:
+                                    if not offices == "none":
+                                        for z in offices[0]:
+                                            self._print_out(z, offices[0][z])
+                            else:
+                                self._print_out(y, term[y])
+                else:
+                    self._print_out(x, node[x])
+
+    def _print_out(self, key, value):
+        print "  %-25s%-15s" % (key, value)

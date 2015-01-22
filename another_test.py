1625e609bca1a2ca39a3fd73dd6c933aee38fd90
Warren <warren.ali@gmail.com>
2015-01-22 13:36:55 +0000

    another test

---
+++ another_test.py
@@ -0,0 +1,23 @@
+from utils import entity_resolver
+
+resolver = entity_resolver.MasterEntitiesResolver()
+
+test_entry = "Nigel Evans"
+test_type = "MP - Member of Parliament"
+test_recipient = None
+
+
+def parse_recipient(entry, entry_type, recipient_type):
+    if entry_type == "MP - Member of Parliament":
+        print "trying mp search"
+        result = resolver.find_mp(entry)
+    elif entry_type == "Political Party" or \
+            recipient_type == "Political Party":
+        result = resolver.find_party(entry)
+    else:
+        result = resolver.get_entities(entry)
+        if result and isinstance(result, list):
+            result = result[0]
+    return result
+
+print parse_recipient(test_entry, test_type, test_recipient)

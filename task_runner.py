7eb6dd9c8641bcc5df78a900e00a7ef112479554
Andy Lulham <a.lulham@gmail.com>
2015-01-29 01:47:59 +0000

    Tidy up task_runner; Add master entities

--- task_runner.py
+++ task_runner.py
@@ -18,9 +18,6 @@ from graphers import graph_lords
 from graphers import graph_mps_interests
 from graphers import graph_party_funding
 
-# master_entities.MasterEntitiesParser().create_mps()
-# master_entities.MasterEntitiesParser().create_lords()
-
 # from data_interfaces import api
 # from data_models import core
 
@@ -33,11 +30,12 @@ from graphers import graph_party_funding
 choices = ['mps', 'lords', 'mps_interests', 'lords_interests', 'party_funding']
 arg_parser = argparse.ArgumentParser(description='Task runner for spud.')
 arg_parser.add_argument('--scrape', nargs='+', choices=choices, help='Specify the scraper(s) to run')
+arg_parser.add_argument('--master', nargs='+', choices=['mps', 'lords'], help='Parse master entities')
 arg_parser.add_argument('--parse', nargs='+', choices=choices, help='Specify the parser(s) to run')
 arg_parser.add_argument('--graph', nargs='+', choices=choices, help='Specify the grapher(s) to run')
 args = arg_parser.parse_args()
 
-if (args.scrape, args.parse, args.graph) == (None, None, None):
+if (args.scrape, args.master, args.parse, args.graph) == (None, None, None, None):
     print 'Nothing to do!'
     arg_parser.print_help()
     exit()
@@ -45,35 +43,43 @@ if (args.scrape, args.parse, args.graph) == (None, None, None):
 # run scrapers
 if args.scrape is not None:
     exec_scraper = {
-        'mps': scrape_mps.MPsInfoScraper(),
-        'lords': scrape_lords.LordsInfoScraper(),
-        'mps_interests': scrape_mps_interests.MPsInterestsScraper(),
-        'lords_interests': scrape_lords_interests.LordsInterestsScraper(),
-        'party_funding': scrape_party_funding.PartyFundingScraper(),
+        'mps': scrape_mps.MPsInfoScraper,
+        'lords': scrape_lords.LordsInfoScraper,
+        'mps_interests': scrape_mps_interests.MPsInterestsScraper,
+        'lords_interests': scrape_lords_interests.LordsInterestsScraper,
+        'party_funding': scrape_party_funding.PartyFundingScraper,
     }
     for scraper in args.scrape:
-        exec_scraper[scraper].run()
+        exec_scraper[scraper]().run()
+
+# parse master entities
+if args.master is not None:
+    master = master_entities.MasterEntitiesParser()
+    if "mps" in args.master:
+        master.create_mps()
+    if "lords" in args.master:
+        master.create_lords()
 
 # run parsers
 if args.parse is not None:
     exec_parser = {
-        'mps': parse_mps.MPsParser(),
-        'lords': parse_lords.LordsParser(),
-        'mps_interests': parse_mps_interests.MPsInterestsParser(),
-        'lords_interests': parse_lords_interests.LordsInterestsParser(),
-        'party_funding': parse_party_funding.PartyFundingParser(),
+        'mps': parse_mps.MPsParser,
+        'lords': parse_lords.LordsParser,
+        'mps_interests': parse_mps_interests.MPsInterestsParser,
+        'lords_interests': parse_lords_interests.LordsInterestsParser,
+        'party_funding': parse_party_funding.PartyFundingParser,
     }
     for parser in args.parse:
-        exec_parser[scraper].run()
+        exec_parser[parser]().run()
 
 # run graphers
 if args.graph is not None:
     exec_grapher = {
-        'mps': graph_mps.GraphMPs(),
-        'lords': graph_lords.GraphLords(),
-        'mps_interests': graph_mps_interests.GraphMPsInterests(),
-        # 'lords_interests': graph_lords_interests.GraphLordsInterests(),
-        'party_funding': graph_party_funding.GraphPartyFunding(),
+        'mps': graph_mps.GraphMPs,
+        'lords': graph_lords.GraphLords,
+        'mps_interests': graph_mps_interests.GraphMPsInterests,
+        # 'lords_interests': graph_lords_interests.GraphLordsInterests,
+        'party_funding': graph_party_funding.GraphPartyFunding,
     }
     for grapher in args.graph:
-        exec_grapher[grapher].run()
+        exec_grapher[grapher]().run()

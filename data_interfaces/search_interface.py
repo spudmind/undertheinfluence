068bddbde1c96618a43bfaf882e984e10ad6cab7
Warren <warren.ali@gmail.com>
2015-02-23 13:23:18 +0000

    udpated search_interface to return our standard api response

--- data_interfaces/search_interface.py
+++ data_interfaces/search_interface.py
@@ -6,7 +6,28 @@ class SearchInterface:
         self._es = Elasticsearch()
         self._db = "spud"
 
-    def search(self, collection, search_query):
-        return self._es.search(
-            index=self._db, doc_type=collection, body=search_query
+    def search(self, field, search):
+        api_collections = ["api_mps", "api_lords", "api_influencers"]
+        search_query = {
+            "query": {
+                "match": {
+                    "{}".format(field): {
+                        "query": u"{}".format(search),
+                        "fuzziness": 2,
+                        "prefix_length": 1
+                    }
+                }
+            }
+        }
+        q = self._es.search(
+            index=self._db, body=search_query, doc_type=api_collections
         )
+        results = [r["_source"] for r in q["hits"]["hits"]]
+        meta = {
+            'total': len(results),
+            'page': 1,
+            'per_page': len(results),
+            'has_more': False,
+        }
+        return results, meta
+

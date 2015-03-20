f8c8bf6cfd98bcfab7f1953996d2563b9a45d119
Warren <warren.ali@gmail.com>
2015-03-20 18:57:06 +0000

    prca_scrape logging for troubleshooting

---
+++ pdf_test.py
@@ -0,0 +1,11 @@
+import scraperwiki
+
+#filename = "/home/warren/git/spud/scrapers/prca/store/agency_2014-09_2014-11.pdf"
+filename = "/root/uti/scrapers/prca/store/agency_2014-09_2014-11.pdf"
+
+with open(filename) as f:
+    pdf_string = f.read()
+# convert to xml
+xml_string = scraperwiki.pdftoxml(pdf_string)
+# parse xml
+print "xml_string:", xml_string

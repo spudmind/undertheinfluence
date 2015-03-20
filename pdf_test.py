3857ea4a51a7407a8ff5a4ac1b3d6b8a8406e6bb
Warren <warren.ali@gmail.com>
2015-03-20 19:05:32 +0000

    prca_scrape logging for troubleshooting

--- pdf_test.py
+++ pdf_test.py
@@ -1,7 +1,7 @@
 import scraperwiki
 
 #filename = "/home/warren/git/spud/scrapers/prca/store/agency_2014-09_2014-11.pdf"
-filename = "/root/uti/scrapers/prca/store/agency_2014-09_2014-11.pdf"
+filename = "/root/uti/scrapers/prca/store/agency_2010-12_2011-02.pdf"
 
 with open(filename) as f:
     pdf_string = f.read()

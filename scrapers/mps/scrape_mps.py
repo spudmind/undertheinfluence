3bd48606c6cdcac9cacca2371f854fed7f30d1ec
Andy Lulham <a.lulham@gmail.com>
2015-03-31 09:01:39 +0100

    Use db entries for scraping MPs; ignore overview

--- scrapers/mps/scrape_mps.py
+++ scrapers/mps/scrape_mps.py
@@ -8,51 +8,48 @@ from utils import mongo
 class ScrapeMPs:
     def __init__(self, **kwargs):
         self._logger = logging.getLogger('spud')
-        self.db = mongo.MongoInterface()
-        # local directory to save fetched files to
-        self.STORE_DIR = "store"
         # get the current path
         self.current_path = os.path.dirname(os.path.abspath(__file__))
+        # database stuff
+        self.db = mongo.MongoInterface()
+        self.PREFIX = "mps"
+        if kwargs["refreshdb"]:
+            self.db.drop("%s_scrape" % self.PREFIX)
 
     def run(self):
-        self._logger.info("Importing MPs")
-        mps = self.get_overview_data()
-        for mp in mps:
-            mp = self.get_mp_details(mp)
-            self.db.save("mps_scrape", mp)
+        self._logger.info("Scraping MPs ...")
+        metas = self.db.fetch_all("%s_fetch" % self.PREFIX, paged=False)
+        for meta in metas:
+            mp = self.get_mp_details(meta)
+            self.db.save("%s_scrape" % self.PREFIX, mp)
+        self._logger.info("Done scraping MPs.")
 
-    def get_overview_data(self):
+    def get_mp_details(self, meta):
         publicwhip_tmpl = u"http://publicwhip.com/mp.php?mpid={0}"
-        with open(os.path.join(self.current_path, self.STORE_DIR, "mps_overview.json")) as f:
-            mps = json.load(f)
-        data = []
-        for mp in mps:
-            self._print_out("MP", mp["name"])
-            self._print_out("Party", mp["party"])
-            self._print_out("person_id", mp["person_id"])
-            data.append({
-                "full_name": mp["name"],
-                "twfy_id": mp["person_id"],
-                "party": mp["party"],
-                "publicwhip_id": mp["member_id"],
-                "publicwhip_url": publicwhip_tmpl.format(mp["person_id"]),
-            })
-            # self._logger.debug("\n")
-        return data
+        with open(os.path.join(self.current_path, meta["filename"])) as f:
+            mp = json.load(f)
+        details = mp.pop("details")
 
-    def get_mp_details(self, mp):
-        with open(os.path.join(self.current_path, self.STORE_DIR, "%s.json" % mp["twfy_id"])) as f:
-            info = json.load(f)
-        details = info.pop("details")
-        mp = dict(mp.items() + info.items())
-
-        image = details[0].get("image")
+        mp["twfy_id"] = details[0]["person_id"]
+        # we set the party and member ID using the most recent
+        # information
         mp["first_name"] = details[0]["first_name"]
         mp["last_name"] = details[0]["last_name"]
+        mp["party"] = details[0]["party"]
+        mp["publicwhip_id"] = details[0]["member_id"]
+        mp["publicwhip_url"] = publicwhip_tmpl.format(mp["twfy_id"])
+
+        mp["source"] = meta["source"]
+
+        mp["aliases"] = []
+        for x in details:
+            full_name = "%s %s" % (x["first_name"], x["last_name"])
+            if full_name not in mp["aliases"]:
+                mp["aliases"].append(full_name)
+
+        image = details[0].get("image")
         mp["image"] = "http://www.theyworkforyou.com%s" % image if image else None
         mp["number_of_terms"] = len(details)
-        self._print_out("first_name", mp["first_name"])
-        self._print_out("last_name", mp["last_name"])
 
         terms = []
         for entry in details:
@@ -69,12 +66,7 @@ class ScrapeMPs:
                     term["offices_held"] = offices
             terms.append(term)
         mp["terms"] = terms
-        #self._report(mp)
         return mp
-        # self._logger.debug("\n\n---")
-
-    def _update_cached_mp(self, id, key, value):
-        self.cache_data.update({"_id": id}, {"$set": {key: value}})
 
     def _get_office(self, positions):
         offices = []
@@ -86,30 +78,6 @@ class ScrapeMPs:
                 office = {"position": position["position"]}
             offices.append(office)
         return offices
-
-    def _report(self, node):
-        for x in node:
-                if x == "terms":
-                    for term in node["terms"]:
-                        self._logger.debug("-")
-                        for y in term:
-                            if y == "offices_held":
-                                offices = term["offices_held"]
-                                if len(offices) > 1 and offices != "none":
-                                    for office in offices:
-                                        for z in office:
-                                            self._print_out(z, office[z])
-                                else:
-                                    if not offices == "none":
-                                        for z in offices[0]:
-                                            self._print_out(z, offices[0][z])
-                            else:
-                                self._print_out(y, term[y])
-                else:
-                    self._print_out(x, node[x])
-
-    def _print_out(self, key, value):
-        self._logger.debug("  %-35s%-25s" % (key, value))
 
 def scrape(**kwargs):
     ScrapeMPs(**kwargs).run()
